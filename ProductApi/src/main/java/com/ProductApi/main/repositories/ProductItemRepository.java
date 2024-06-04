55eda21f7dc0ce42ddeabe52803f280a1fa06f3a
LuizPauloMS <125107237+LuizPauloMS@users.noreply.github.com>
2024-06-03 22:17:04 -0300

    Delete ProductApi directory

--- ProductApi/src/main/java/com/ProductApi/main/repositories/ProductItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ProductApi.main.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ProductApi.main.entity.ProductItem;
-
-public interface ProductItemRepository extends JpaRepository<ProductItem, String>{
-
-}
