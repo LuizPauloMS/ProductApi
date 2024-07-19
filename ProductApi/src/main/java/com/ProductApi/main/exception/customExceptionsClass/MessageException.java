c9f69dd072e32da4b9572f8f6105113f5317da51
LuizPauloMS <125107237+LuizPauloMS@users.noreply.github.com>
2024-07-18 23:15:25 -0300

    Delete ProductApi directory

--- ProductApi/src/main/java/com/ProductApi/main/exception/customExceptionsClass/MessageException.java
+++
@@ -1,8 +0,0 @@
-package com.ProductApi.main.exception.customExceptionsClass;
-
-import java.util.Map;
-
-public interface MessageException {
-	 String getExceptionKey();
-	 Map<String, Object> getMapDetails();
-}
